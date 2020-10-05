115931ab860df099ed726f9785a6972a16458bdf
Shamal Sandeep <ssdh.gdrive@gmail.com>
2020-10-05 21:43:42 +0530

    Trial balance reset at April 1st

--- pkg/sql/queries/queries.go
+++ pkg/sql/queries/queries.go
@@ -517,14 +517,29 @@ const TRANSACTION = `
 `
 
 const TRIAL_BALANCE = `
-	SELECT A.id, A.account_id, A.name, COALESCE(AT.debit, 0) AS debit, COALESCE(AT.credit, 0) AS credit, COALESCE(AT.debit-AT.credit, 0) AS balance
+	(SELECT A.id, A.account_id, A.name, COALESCE(AT.debit, 0) AS debit, COALESCE(AT.credit, 0) AS credit, COALESCE(AT.debit-AT.credit, 0) AS balance
 	FROM account A
 	LEFT JOIN (
 		SELECT AT.account_id, SUM(CASE WHEN AT.type = "DR" THEN AT.amount ELSE 0 END) AS debit, SUM(CASE WHEN AT.type = "CR" THEN AT.amount ELSE 0 END) AS credit 
-		FROM account_transaction AT
+		FROM (SELECT AT.* FROM account_transaction AT LEFT JOIN transaction T ON T.id = AT.transaction_id) AT
 		GROUP BY AT.account_id
 	) AT ON AT.account_id = A.id
-	ORDER BY account_id ASC
+	LEFT JOIN account_category AC ON AC.id = A.account_category_id
+	LEFT JOIN sub_account SA ON SA.id = AC.sub_account_id
+	LEFT JOIN main_account MA ON MA.id = SA.main_account_id
+	WHERE MA.name IN ('Assets', 'Equity', 'Liabilities'))
+	UNION	
+	(SELECT A.id, A.account_id, A.name, COALESCE(AT.debit, 0) AS debit, COALESCE(AT.credit, 0) AS credit, COALESCE(AT.debit-AT.credit, 0) AS balance
+	FROM account A
+	LEFT JOIN (
+		SELECT AT.account_id, SUM(CASE WHEN AT.type = "DR" THEN AT.amount ELSE 0 END) AS debit, SUM(CASE WHEN AT.type = "CR" THEN AT.amount ELSE 0 END) AS credit 
+		FROM (SELECT AT.* FROM account_transaction AT LEFT JOIN transaction T ON T.id = AT.transaction_id WHERE T.posting_date >= CONCAT(YEAR(NOW()), '-04-01')) AT
+		GROUP BY AT.account_id
+	) AT ON AT.account_id = A.id
+	LEFT JOIN account_category AC ON AC.id = A.account_category_id
+	LEFT JOIN sub_account SA ON SA.id = AC.sub_account_id
+	LEFT JOIN main_account MA ON MA.id = SA.main_account_id
+	WHERE MA.name IN ('Cost of Sales', 'Expenses', 'Other Revenue', 'Revenue'))
 `
 
 const OFFICER_ACC_NO = `
